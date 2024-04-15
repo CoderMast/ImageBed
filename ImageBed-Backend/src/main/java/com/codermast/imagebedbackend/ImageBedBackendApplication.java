93d990cda2380451a96c2265f3c837e7526b6a39
codermast <codermast@qq.com>
2024-04-15 11:49:12 +0800

    Image Bed Project Initilizer

---
+++ ImageBed-Backend/src/main/java/com/codermast/imagebedbackend/ImageBedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codermast.imagebedbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageBedBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageBedBackendApplication.class, args);
+    }
+
+}
---
+++ ImageBed-Backend/src/test/java/com/codermast/imagebedbackend/ImageBedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codermast.imagebedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageBedBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
